a7cdd754d260c704c9127d80731b69652688b6f0
felixinforest <w.natnichaza@gmail.com>
2025-06-25 09:32:20 +0700

    add skip_basecaller

--- assest/command.sh
+++ assest/command.sh
@@ -1,1 +1,4 @@
-bash /data/Basecaller/dorado-0.9.0-linux-x64/script/dna_dorado090.sh -i $PWD/POD5 -o $PWD -s doradoSheet.csv -m sup@v5.0.0 -k SQK-RBK114-96 -a --no-trim -d ' ' &> command.log
+# Default
+bash /data/Basecaller/dorado-0.9.0-linux-x64/script/dna_dorado090.sh -i $PWD/POD5 -o $PWD -s doradoSheet.csv -m sup@v5.0.0 -k SQK-RBK114-96 -a --no-trim -d ' '
+# Skip_basecaller
+bash /data/Basecaller/dorado-0.9.0-linux-x64/script/dna_dorado090.sh -i $PWD/POD5 -o $PWD -s doradoSheet.csv -m sup@v5.0.0 -k SQK-RBK114-96 -a --no-trim -d ' ' --skip_basecaller
